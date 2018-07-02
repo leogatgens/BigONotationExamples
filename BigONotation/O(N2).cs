8711781714783ec22e0361e45779affdf4427efc
Leonardo Meléndez <leogatgens@gmail.com>
2018-07-02 16:53:18 -0600

    More options to test

---
+++ BigONotation/O(N2).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigONotation
+{
+    class O_N2_
+    {
+
+       public  bool ContainsDuplicates(String[] strings)
+        {
+            for (int i = 0; i < strings.Length; i++)
+            {
+                for (int j = 0; j < strings.Length; j++)
+                {
+                    if (i == j) // Don't compare with self
+                    {
+                        continue;
+                    }
+
+                    if (strings[i] == strings[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- BigONotation/Program.cs
+++ BigONotation/Program.cs
@@ -8,42 +8,94 @@ namespace BigONotation
 {
     class Program
     {
-      
+        private static string[] ListOfOneMillonItems;
+        private static string[] ListOf10000Items;
+
 
         static void Main(string[] args)
         {
-          
+            ListOfOneMillonItems = GenerateOneMillonItems();
+            ListOf10000Items = Generate10000Items();
+
             SameTimeBigList();
-            SameTimeTest();
+            SameTimeTestSmallList();
+            
+            GrowLinearly1();
+            GrowLinearly2();
+
+            SquareOfTheSize1();
+            SquareOfTheSize2();
 
             Console.ReadKey();
         }
 
-        private static void SameTimeTest()
+        private static string[] GenerateOneMillonItems()
         {
-       
+            string[] TheOneMillonItems = new string[1000000] ;
+            string Prefix = "Item";
+            for (int i = 0; i < 1000000; i++)
+            {
+                TheOneMillonItems[i] = string.Concat(Prefix, i.ToString());
 
+            }
+
+            return TheOneMillonItems;
+        }
+
+        private static string[] Generate10000Items()
+        {
+            string[] The10000Items = new string[10000];
+            string Prefix = "Item";
+            for (int i = 0; i < 10000; i++)
+            {
+                The10000Items[i] = string.Concat(Prefix, i.ToString());
+
+            }
+
+            return The10000Items;
+        }
+
+
+        private static void GrowLinearly1()
+        {
             PerformanceGrowLinearly performanceGrowLinearly = new PerformanceGrowLinearly();
-            string[] itemsSmall = {
-                "Item1", "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item1", "Item2", "Item3", "Item4", "Item2", "Item3",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                 };
-            
-            SameTime sameTime = new SameTime();
-
-
-            TimeDiffCalculator timerDiff = new TimeDiffCalculator("Small");
-
-
+            TimeDiffCalculator timerDiff = new TimeDiffCalculator("GrowLinearly1");
 
             timerDiff.Start();
-            sameTime.IsFirstElementNull(itemsSmall);
+            bool ItemFound = performanceGrowLinearly.ContainsValue(ListOf10000Items, "Item9999");
             timerDiff.Finish();
-     
+            if (ItemFound == true)
+            {
+                Console.WriteLine("Item9999 exist.");
+
+            }
+                
+
+        }
+
+        private static void GrowLinearly2()
+        {
+            PerformanceGrowLinearly performanceGrowLinearly = new PerformanceGrowLinearly();
+            TimeDiffCalculator timerDiff = new TimeDiffCalculator("GrowLinearly2");
+         
+            timerDiff.Start();
+            bool ItemFound = performanceGrowLinearly.ContainsValue(ListOfOneMillonItems, "Item999999");
+            timerDiff.Finish();
+            if (ItemFound == true)
+            {
+                Console.WriteLine("Item999999 exist.");
+
+            }
+        }
+
+        private static void SameTimeTestSmallList()
+        {   
+                   
+            SameTime sameTime = new SameTime();
+            TimeDiffCalculator timerDiff = new TimeDiffCalculator("Small");
+            timerDiff.Start();
+            sameTime.IsFirstElementNull(ListOf10000Items);
+            timerDiff.Finish();   
 
            
         }
@@ -52,89 +104,50 @@ namespace BigONotation
         {
           
 
-            PerformanceGrowLinearly performanceGrowLinearly = new PerformanceGrowLinearly();
-           
-            string[] itemsBig = {
-                "Item1", "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"
-                , "Item2", "Item3", "Item4", "Item1",
-                "Item2", "Item3", "Item4", "Item2", "Item3", "Item4",
-                "Item2", "Item3", "Item4", "Item1", "Item2", "Item3",
-                "Item4", "Item2", "Item3", "Item4", "Item1", "Item2",
-                "Item4", "Item1", "Item2", "Item3", "Item4", "Item2",
-                "Item3", "Item4", "Item1", "Item2", "Item3", "Item4"};
             SameTime sameTime = new SameTime( );
             TimeDiffCalculator timerDiff = new TimeDiffCalculator("Big");
 
-
-
             timerDiff.Start();
-            sameTime.IsFirstElementNull(itemsBig);
+            sameTime.IsFirstElementNull(ListOfOneMillonItems);
             timerDiff.Finish();
         }
+
+
+        private static void SquareOfTheSize1()
+        {
+            O_N2_ performanceGrowLinearly = new O_N2_();
+            TimeDiffCalculator timerDiff = new TimeDiffCalculator("SquareOfTheSize1");
+       
+            timerDiff.Start();
+         bool isDuplicated =   performanceGrowLinearly.ContainsDuplicates (ListOf10000Items);
+            timerDiff.Finish();
+            if (isDuplicated)
+            {
+                Console.WriteLine("Item duplicated");
+            }
+            else {
+                Console.WriteLine("Item not duplicated");
+            }
+        }
+
+        private static void SquareOfTheSize2()
+        {
+            O_N2_ performanceGrowLinearly = new O_N2_();
+            TimeDiffCalculator timerDiff = new TimeDiffCalculator("SquareOfTheSize2");
+           
+            timerDiff.Start();
+            bool isDuplicated = performanceGrowLinearly.ContainsDuplicates(ListOfOneMillonItems);
+            timerDiff.Finish();
+            if (isDuplicated)
+            {
+                Console.WriteLine("Item duplicated");
+            }
+            else
+            {
+                Console.WriteLine("Item not duplicated");
+            }
+        }
+
+
     }
 }
